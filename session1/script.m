46d82d8d01ba907dfb69ffad2087662e14240140
Doinakis <doinakis.michalis@gmail.com>
2021-03-27 20:09:06 +0200

    ADD: session1 tests and reports.

---
+++ session1/script.m
@@ -0,0 +1,79 @@
+%%
+% Diktya 1 Doinakis Michalis 9292
+% e-mail: doinakis@ece.auth.gr
+%%
+clc
+clear
+close all
+%%
+% Request Codes
+echo = "E4360.";
+ack = "Q7000,";
+nack = "R7611.";
+%%
+% Produces graph G1 with the response times in milliseconds for a 4 minute
+% experiment
+%%
+echoExperiment = importdata("echoExperiment.csv");
+G1 = figure;
+bar(echoExperiment);
+g1Title = "G1: Echo Request Code: " + echo + newline + "Echo Packet experiment started: 2021-03-26 at 20:47:50 EET" + ...
+    newline + "Echo Packet experiment ended: 2021-03-26 at 20:51:50 EET.";
+title(g1Title);
+ylabel("Response time (in milliseconds)");
+xlabel("Packet");
+saveas(G1,"G1.jpg");
+%%
+% Produces graph G2 with the response times in milliseconds for a 4 minute
+% experiment (Automatic Repeat Request)
+arqResponseTimes = importdata("arqResponseTimes.csv");
+G2 = figure;
+bar(arqResponseTimes);
+g2Title = "G2: ACK Request Code: " + ack + "NACK Request Code: " + nack + newline + ...
+    "Echo Packet experiment started: 2021-03-26 at 20:47:50 EET" + newline + ...
+    "Echo Packet experiment ended: 2021-03-26 at 20:51:50 EET.";
+title(g2Title);
+ylabel("Response time (in milliseconds)");
+xlabel("Packet");
+saveas(G2,"G2.jpg");
+%%
+% Finds the estimation of the distribution for the repeations
+ArqNumberOfNack = importdata("ArqNumberOfNack.csv");
+ArqNumberOfNack = ArqNumberOfNack(ArqNumberOfNack ~=0);
+timesRequested = unique(ArqNumberOfNack);
+pd = fitdist(ArqNumberOfNack','Exponential');
+xgrid = linspace(0,5,100)';
+pdfEst = pdf(pd,xgrid);
+G3 = figure;
+h = histogram(ArqNumberOfNack,'Normalization','pdf');
+line(xgrid+1,pdfEst);
+ylim([0 1]);
+xticks(1:size(timesRequested,2));
+g3Title = "Distribution estimation." + newline + "G3: ACK Request Code: " + ack + "NACK Request Code: " + nack + ...
+    newline + "Starting Automatic Repeat experiment: 2021-03-26 at 20:52:27 EET" + newline + ...
+    "Automatic Repeat Request experiment ended: 2021-03-26 at 20:56:27 EET";
+title(g3Title);
+xlabel("#Repeations");
+ylabel("Relative frequency of repeations");
+saveas(G3,"G3.jpg");
+%%
+% Calculation of Bit Error Rate
+% P = (1-BER)^L <=> BER = 1 - P^(1/L)
+%%
+ArqNumberOfNack = importdata("ArqNumberOfNack.csv");
+k = 0;
+L = 16 * 8;
+ArqNumberOfNack = ArqNumberOfNack(ArqNumberOfNack ~=0);
+timesRequested = unique(ArqNumberOfNack);
+
+% k = 1*times(1) + 2*times(2) + ... + n*times(n)
+for i= 1:size(timesRequested,2)
+    if timesRequested(i) == 0
+        continue;
+    end
+    k = k + timesRequested(i) * sum(ArqNumberOfNack == timesRequested(i));
+end
+
+P = size(ArqNumberOfNack,2)/k;
+BER = 1 - P^(1/L);
+%%
